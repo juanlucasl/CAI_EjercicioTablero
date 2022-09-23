7d9b79ec2a82b0e3894cbe213e653e02f265d882
Juan L <33398520+juanlucasl@users.noreply.github.com>
2022-09-23 20:25:08 -0300

    Implementar Clase Tablero

--- ProyectoTablero.Servicios/Tablero.cs
+++ ProyectoTablero.Servicios/Tablero.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace ProyectoTablero.Servicios
 {
@@ -12,13 +14,59 @@ namespace ProyectoTablero.Servicios
             _descripcion = descripcion;
             _fechaInicioProyecto = DateTime.Now;
         }
-        
+
         // Atributos
 
         private static int _codigoContador = 0;
-        private string _titulo;
-        private string _descripcion;
-        private List<Tarea> _tareas;
-        private DateTime _fechaInicioProyecto;
+        private readonly string _titulo;
+        private readonly string _descripcion;
+        private readonly List<Tarea> _tareas;
+        private readonly DateTime _fechaInicioProyecto;
+
+        // Metodos
+
+        /// <summary>Crea una nueva tarea y la agrega al tablero.</summary>
+        /// <param name="descripcion">Descripcion de la tarea.</param>
+        /// <param name="orden">Orden de la Tarea. Por defecto equivale a la cantidad de tareas en el tablero (incluyendo a la nueva)</param>
+        public void AgregarTarea(string descripcion, int orden = -1)
+        {
+            int ordenTarea = orden < 0 ? _tareas.Count() + 1 : orden;
+            _tareas.Add(new Tarea(descripcion, ordenTarea, Interlocked.Increment(ref _codigoContador)));
+        }
+
+        /// <summary>Cambia el estado de una tarea.</summary>
+        /// <param name="codigo">Codigo de la tarea a modificar.</param>
+        /// <param name="estado">Nuevo estado.</param>
+        public void CambiarEstado(int codigo, Estado estado)
+        {
+            int indiceTarea = ObtenerTareaPorCodigo(codigo);
+            _tareas[indiceTarea].Estado = estado;
+            _tareas[indiceTarea].FechaRealizacion = DateTime.Now;
+        }
+
+        /// <summary>Devuelve todas las tareas con el estado dado. Si no se especifica un estado, devuelve todas las tareas.</summary>
+        /// <param name="estado">Estado a buscar.</param>
+        /// <returns>Lista de tareas.</returns>
+        public IEnumerable<Tarea> TraerTareas(Estado? estado = null)
+        {
+            if (estado == null) return _tareas;
+            return _tareas.FindAll((tarea) => (tarea.Estado == estado));
+        }
+        
+        /// <summary>Devuelve la tarea mas antigua.</summary>
+        /// <returns>Tarea mas antigua.</returns>
+        public Tarea TraerTareaMasAntigua()
+        {
+            IOrderedEnumerable<Tarea> tareasOrdenadas = _tareas.OrderBy((tarea) => tarea.FechaAlta);
+            return tareasOrdenadas.First();
+        }
+
+        /// <summary>Devuelve el indice de la tarea correspondiente a un codigo dado.</summary>
+        /// <param name="codigo">Codigo de tarea.</param>
+        /// <returns>Indice de la tarea encontrada o -1.</returns>
+        private int ObtenerTareaPorCodigo(int codigo)
+        {
+            return _tareas.FindIndex((tarea) => (tarea.Codigo == codigo));
+        }
     }
 }
--- ProyectoTablero.Servicios/Tarea.cs
+++ ProyectoTablero.Servicios/Tarea.cs
@@ -5,23 +5,46 @@ namespace ProyectoTablero.Servicios
     public class Tarea
     {
         // Constructor
-        public Tarea(string descripcion, int orden)
+        public Tarea(string descripcion, int orden, int codigo)
         {
             _descripcion = descripcion;
             _orden = orden;
-            _estado = Estado.Nueva;
+            _codigo = codigo;
             _fechaAlta = DateTime.Now;
+            _estado = Estado.Nueva;
+
         }
 
         // Atributos
-        private int _codigo;
-        private string _descripcion;
-        private int _orden;
-        private Estado _estado;
-        private DateTime _fechaAlta;
+        private readonly string _descripcion;
+        private readonly int _orden;
+        private readonly int _codigo;
+        private readonly DateTime _fechaAlta;
         private DateTime _fechaRealizacion;
+        private Estado _estado;
 
         // Propiedades
+        internal int Codigo
+        {
+            get { return _codigo; }
+        }
+
+        internal DateTime FechaAlta
+        {
+            get { return _fechaAlta; }
+        }
+
+        internal DateTime FechaRealizacion
+        {
+            set { _fechaRealizacion = value; }
+        }
+        
+        internal Estado Estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
+
         public bool IsFinalizada
         {
             get { return _estado == Estado.Finalizada; }
